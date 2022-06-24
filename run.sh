404f5f0503890412c30f97c63ccdf90c170a2ab1
chenxusheng <812747475@qq.com>
2022-06-24 16:39:16 +0800

    初始化

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source venv/bin/activate
-
+t_start=`date '+%s'`
 totalProcess=$1
 batchsize=$2
 fieldNum=$3
@@ -16,7 +16,9 @@ do
   python main.py $i $batchsize $fieldNum &
 done
 wait
+# 清理
 rm data.csv
+rm faker.log
 # 生成csv header
 schema[0]="id"
 for i in `seq 1 $fieldNum`
@@ -34,11 +36,19 @@ do
  schema2[$i]="f$i string"
 done
 schemaList=$(IFS=,; printf %s "${schema2[*]}")
-if [ $((batchsize/10000)) -gt 0 ];then
- posix="$((batchsize/10000))w"
+totalRows=$((totalProcess*batchsize))
+if [ $((totalRows/10000)) -gt 0 ];then
+ posix="$((totalRows/10000))w"
+ if [ $((totalRows%10000)) -gt 0 ];then
+   posix="${posix}$((totalRows%10000))"
+ fi
 else
  posix=$batchsize
 fi
-echo "create table t_f${fieldNum}_r$posix ($schemaList)"
-
-echo '完成'
+tableName="t_f${fieldNum}_r$posix"
+echo "进程数：$totalProcess 数据量$((totalProcess*batchsize))：字段数：$fieldNum" >> faker.log
+echo "create table $tableName ($schemaList) row format delimited fields terminated by ',';" >> faker.log
+echo "load data local inpath '`pwd`/data.csv' into table $tableName;" >>faker.log
+t_end=`date '+%s'`
+echo "完成,耗时:$((t_end-t_start))s" >> faker.log
+cat faker.log
